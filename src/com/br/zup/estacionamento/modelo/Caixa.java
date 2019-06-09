0197b4722c9e1ea539c095c28298d8c3dd49d3ae
Will <will@gmail.com>
2019-06-09 16:29:02 -0300

    Calculando horasno parking

--- src/com/br/zup/estacionamento/modelo/Caixa.java
+++ src/com/br/zup/estacionamento/modelo/Caixa.java
@@ -16,12 +16,28 @@ public class Caixa {
 	public double calcularQuantoVouPagarNoEstacionamento(double minutos) {
 		double valorAPagar = 0;
 		//logica para saber quanto temos que pagar
+		//15 minutos de tolerancia sem pagar
+		//primeira hora tem o valor de 10 reais
+		//+ 5 reais por hora
+		
+		if(minutos <= 15) {//Menos de 15 mins
+			valorAPagar = 0;
+		}else if(minutos > 15 && minutos <= 60) {//Entre 15 e 60
+			valorAPagar = 10;
+		}else { //Mais de 60 mins
+			//61, 62, 63 ... 110, 111.. 120 .... 180
+			valorAPagar = 10;
+			double horas = transformarMinutosEmHora(minutos);
+			horas = horas - 1;
+			valorAPagar = valorAPagar + horas * 5;
+		}
 		return valorAPagar;
 	}
 	
-	public double transformarMinutosEmHora(double minutos) {
+	public long transformarMinutosEmHora(double minutos) {
+		//56.89(double, float) => 56(Inteiro[byte, short, int, long])
 		double horas = minutos/60;
-		return horas;
+		return (long) horas;
 	}
 	
 	
